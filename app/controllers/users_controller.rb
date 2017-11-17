0451f772810dcbde1a48538c435f8ee74808051e
Joanna <jmgsese@gmail.com>
2017-11-17 13:53:12 -0600

    Add bcrypt'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,25 @@ class UsersController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    #add if logged in logic
+    @user = User.create(user_params)
+  end
+
   def show
     @user = User.find_by(id: params[:id])
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
