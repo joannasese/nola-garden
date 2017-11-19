9633f456683334c8173619895eb6e6de8fda50a7
Joanna <jmgsese@gmail.com>
2017-11-19 17:57:20 -0600

    Link new plant form on user show page

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,14 +1,20 @@
 class PlantsController < ApplicationController
+  before_action :require_login
+
   def new
+    @plant = Plant.new
   end
 
   def create
+
   end
 
   def edit
   end
 
   def show
+    @plant = Plant.find_by(id: params[:id])
+    # @user = User.find_by(id: params[:id])
   end
 
   def update
