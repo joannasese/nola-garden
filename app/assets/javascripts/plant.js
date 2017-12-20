bd4c1e6570f564aa1917a06134cae73c0483e74c
Joanna <jmgsese@gmail.com>
2017-12-20 09:58:19 -0600

    Add jquery gem and set up javascript usage

--- app/assets/javascripts/plant.js
+++ app/assets/javascripts/plant.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
-  $("#main-content").append("Can you see me? I was added with jQuery.")
-});
+function testComment(){
+  $('.title-link').on('click', function(event){
+    console.log('Hey')
+    $("#menu-container").append("Can you see me? I was added with jQuery.");
+    event.preventDefault();
+  })
+}
+// })
+// $('#my-link').click(function (event) {
+//   alert('Hooray!');
+//   event.preventDefault(); // Prevent link from following its href
+// });
